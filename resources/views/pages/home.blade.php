514d30b6f4238171716d64054e959ee08a32e760
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-09-28 18:40:04 +0200

    Update home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.main')
 
 @section('content')
-    
+    <!-- background-image -->
+    <div class="background-image">
+        <img src="{{ asset('img/background.jpg') }}" alt="background">
+        <div class="block-text">
+            <h1 class="heading">Laravel Blog</h1>
+            <p class="lead">Welcome to the Laravel blog. Here you can write and view blog posts, comments and like comments.</p>
+            <a href="#" class="btn btn-primary">Read more</a>
+        </div>
+    </div>
+    <!-- !background-image -->
 @endsection
