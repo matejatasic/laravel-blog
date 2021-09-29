14f603105ca7958e26bca968aa6c50cc8248aaed
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-09-29 17:12:44 +0200

    Update 2021_09_29_095623_create_posts_table.php

--- database/migrations/2021_09_29_095623_create_posts_table.php
+++ database/migrations/2021_09_29_095623_create_posts_table.php
@@ -18,6 +18,7 @@ class CreatePostsTable extends Migration
             $table->string('title');
             $table->text('body');
             $table->integer('user_id');
+            $table->string('img_path');
             $table->timestamps();
         });
     }
