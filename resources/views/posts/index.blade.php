a77e94ea3995be4df2f9563050198af06525136d
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-09-29 18:37:13 +0200

    Create index.blade.php

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container">
+        <div class="mb-2">
+            <h1 class="text-center large">Blog Posts</h1>
+        </div>
+        <hr>
+        <div class="my-3">
+            @foreach ($posts as $post)
+                <div class="blog-post mb-3">
+                    <div>
+                        <img src="{{ $post['img_path'] }}" alt="post_image">
+                    </div>
+                    <div class="px-2">
+                        <h2 class="medium">{{ $post['title'] > 20 ? substr($post['title'], 0, 20) . '...' : $post['title'] }}</h2>
+                        <p class="mb-2">By <b>Some Author</b>, {{ $post->created_at->diffForHumans() }}</p>
+                        <p class="lead">
+                            {{ strlen($post['body']) > 150 ? substr($post['body'], 0, 150) . '...' : $post['body'] }}
+                        </p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
