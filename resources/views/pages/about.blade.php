09d11a9655368586a8eb9b4d853efa10f5907324
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-09-29 09:50:04 +0200

    Create about.blade.php

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container">
+        <div class="mb-2">
+            <h1 class="text-center large">About us</h1>
+        </div>
+        <hr>
+        <div class="py-5">
+            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ea sapiente optio quam. 
+            Odio in sed hic unde illum? Temporibus saepe modi quidem debitis eligendi velit repellendus expedita dolorum 
+            minima dignissimos dolor sed dicta corporis, perferendis facere soluta pariatur minus totam rem incidunt qui 
+            voluptatum possimus voluptas doloribus! Exercitationem aut repellendus culpa, in qui facilis maiores, 
+            tempore nesciunt ducimus ipsa quaerat, deserunt error nisi tempora nam laborum cumque sunt velit cupiditate totam! 
+            Voluptas cupiditate fugiat, blanditiis adipisci perspiciatis veritatis eos.</p>
+        </div>
+        <div class="about-background about-background-1"></div>
+        <div class="py-5">
+            <h2 class="text-center medium">Our Values</h2>
+            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ea sapiente optio quam. 
+            Odio in sed hic unde illum? Temporibus saepe modi quidem debitis eligendi velit repellendus expedita dolorum 
+            minima dignissimos dolor sed dicta corporis, perferendis facere soluta pariatur minus totam rem incidunt qui 
+            voluptatum possimus voluptas doloribus! Exercitationem aut repellendus culpa, in qui facilis maiores, 
+            tempore nesciunt ducimus ipsa quaerat, deserunt error nisi tempora nam laborum cumque sunt velit cupiditate totam! 
+            Voluptas cupiditate fugiat, blanditiis adipisci perspiciatis veritatis eos.</p>
+        </div>
+        <div class="about-background about-background-2"></div>
+        <div class="py-5">
+            <h2 class="text-center medium">Our Goals</h2>
+            <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ea sapiente optio quam. 
+            Odio in sed hic unde illum? Temporibus saepe modi quidem debitis eligendi velit repellendus expedita dolorum 
+            minima dignissimos dolor sed dicta corporis, perferendis facere soluta pariatur minus totam rem incidunt qui 
+            voluptatum possimus voluptas doloribus! Exercitationem aut repellendus culpa, in qui facilis maiores, 
+            tempore nesciunt ducimus ipsa quaerat, deserunt error nisi tempora nam laborum cumque sunt velit cupiditate totam! 
+            Voluptas cupiditate fugiat, blanditiis adipisci perspiciatis veritatis eos.</p>
+        </div>
+    </div>
+@endsection
