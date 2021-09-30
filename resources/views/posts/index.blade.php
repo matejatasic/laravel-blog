bbce16feb02022d549f7aba084fcba885a078418
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-09-30 09:12:56 +0200

    Update index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,16 +8,17 @@
         <hr>
         <div class="my-3">
             @foreach ($posts as $post)
-                <div class="blog-post mb-3">
+                <div class="blog-post mb-4">
                     <div>
-                        <img src="{{ $post['img_path'] }}" alt="post_image">
+                        <img src="{{ $post->img_path }}" alt="post_image">
                     </div>
-                    <div class="px-2">
-                        <h2 class="medium">{{ $post['title'] > 20 ? substr($post['title'], 0, 20) . '...' : $post['title'] }}</h2>
+                    <div class="px-3">
+                        <h2 class="medium">{{ $post->title > 20 ? substr($post->title, 0, 20) . '...' : $post->title }}</h2>
                         <p class="mb-2">By <b>Some Author</b>, {{ $post->created_at->diffForHumans() }}</p>
                         <p class="lead">
-                            {{ strlen($post['body']) > 150 ? substr($post['body'], 0, 150) . '...' : $post['body'] }}
+                            {{ strlen($post->body) > 150 ? substr($post->body, 0, 150) . '...' : $post->body }}
                         </p>
+                        <a href="{{ route('posts.show', $post->id) }}" class="btn btn-primary">Read More</a>
                     </div>
                 </div>
             @endforeach
