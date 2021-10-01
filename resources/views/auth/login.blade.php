ee3b918eb17be1b96f3c276825b593ad4cbbc76b
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-10-01 11:10:58 +0200

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,18 +2,28 @@
 
 @section('content')
     <div class="container">
+        @if ($errors->any())
+            <div class="alert alert-danger" role="alert">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <div class="login-form">
             <div class="form-heading">
                 Login
             </div>
-            <form action="">
+            <form action="{{ route('postLogin') }}" method="POST">
+                @csrf
                 <div class="form-group">
                     <label>Email</label>
                     <input type="text" name="email">
                 </div>
                 <div class="form-group">
                     <label>Password</label>
-                    <input type="text" name="email">
+                    <input type="password" name="password">
                 </div>
                 <div>
                     <input type="submit" class="btn btn-success mx-2" value="Login">
