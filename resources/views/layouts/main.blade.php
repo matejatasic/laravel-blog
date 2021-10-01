8a5c47bab00c69a15b72b25ad3000e57c5bd9e07
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-10-01 11:10:29 +0200

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,6 +8,12 @@
 
         <!-- Custom styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        
+        
+        @if (Auth::check())
+            <!-- Font awesome -->
+            <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+        @endif
     </head>
     <body>
         <!-- navbar -->
@@ -29,10 +35,18 @@
                     <a href="{{ route('pages.contact') }}">Contact</a>
                 </li>
             </ul>
-            <ul class="right-nav">
-                <li><a href="#">Login</a></li>
-                <li><a href="#">Register</a></li>
-            </ul>
+            @if (Auth::check())
+                <div class="right-nav">
+                    <div class="user-name">
+                        <a href="#">{{ auth()->user()->name }}</a> <i class="fas fa-caret-down"></i>
+                    </div>
+                </div>
+            @else
+                <ul class="right-nav">
+                    <li><a href="{{ route('getLogin') }}">Login</a></li>
+                    <li><a href="{{ route('getRegister') }}">Register</a></li>
+                </ul>
+            @endif
         </nav>
         <!-- !navbar -->
 
