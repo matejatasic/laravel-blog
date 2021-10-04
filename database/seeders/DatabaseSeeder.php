440db951e99b290be1b85495bd2562481608b3bc
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-10-04 17:11:26 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(PostSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(PostSeeder::class);
         $this->call(CommentSeeder::class);
     }
 }
