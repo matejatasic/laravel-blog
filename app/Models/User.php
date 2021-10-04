00fab1205e768b468b86ff71a12a6d4bea0a8a8f
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-10-04 15:51:03 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $table = 'users';
     /**
      * The attributes that are mass assignable.
      *
@@ -41,4 +42,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts() {
+        return $this->hasMany('App\Models\Post');
+    }
+
+    public function comments() {
+        return $this->hasMany('App\Models\Comment');
+    }
+    
+    public function likes() {
+        return $this->hasMany('App\Models\Like');
+    }
 }
