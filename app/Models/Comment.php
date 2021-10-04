145e650132f7c4168e72a8f5ca1cc6254164d035
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-10-04 15:56:35 +0200

    Update Comment.php

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Like;
 
 class Comment extends Model
 {
     use HasFactory;
+
+    public function likedBy(User $user) {
+        $this->likes->contains('user_id', $user_id);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function post() {
+        return $this->belongsTo(Post::class);
+    }
+    
+    public function likes() {
+        return $this->hasMany(Like::class);
+    }
 }
