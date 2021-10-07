9852c92cf5fa6841830ad46de56ce5d4250b5280
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-10-07 12:34:21 +0200

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Comment;
+use App\Models\Like;
 
 class AdminController extends Controller
 {
-    public function index() {
-        return view('admin.index');
+    public function dashboard() {
+        $posts = Post::all();
+        $comments = Comment::all();
+        $likes = Like::all();
+
+        return view('admin.dashboard', [
+            'posts' => $posts,
+            'comments' => $comments,
+            'likes' => $likes,
+        ]);
     }
 }
