6ec16b112d0f946e89da429bc1c980ee42824e8b
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-10-08 12:29:56 +0200

    Add files via upload

---
+++ resources/views/admin/categories.blade.php
@@ -0,0 +1,38 @@
+@extends('admin.layout.main')
+
+@section('content')
+    <div class="mb-2">
+        <h1 class="text-center large">Categories</h1>
+    </div>
+    <hr>
+    <div class="my-3">
+        @if (Session::has('success'))
+            <div class="alert alert-success" role="alert">
+                {{ Session::get('success') }}
+            </div>
+        @endif
+        <table id="posts-table" class="mb-2">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <td>Posts</td>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($categories as $category)
+                    <tr>
+                        <td>{{ $category->id }}</td>
+                        <td>{{ $category->name }}</td>
+                        <td>{{ count($category->posts) }}</td>
+                        <td>
+                            <button class="btn btn-primary">Edit</button>
+                            <button class="btn btn-danger">Delete</button>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        {{ $categories->links('vendor.custom') }}
+    @endsection
---
+++ resources/views/admin/tags.blade.php
@@ -0,0 +1,38 @@
+@extends('admin.layout.main')
+
+@section('content')
+    <div class="mb-2">
+        <h1 class="text-center large">Tags</h1>
+    </div>
+    <hr>
+    <div class="my-3">
+        @if (Session::has('success'))
+            <div class="alert alert-success" role="alert">
+                {{ Session::get('success') }}
+            </div>
+        @endif
+        <table id="posts-table" class="mb-2">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Posts</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tags as $tag)
+                    <tr>
+                        <td>{{ $tag->id }}</td>
+                        <td>{{ $tag->name }}</td>
+                        <td>{{ count($tag->posts) }}</td>
+                        <td>
+                            <button class="btn btn-primary">Edit</button>
+                            <button class="btn btn-danger">Delete</button>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        {{ $tags->links('vendor.custom') }}
+    @endsection
