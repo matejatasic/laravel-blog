6736ca711fe473752540fd95fba106171f5143c0
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-10-08 11:21:14 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(TagSeeder::class);
+        $this->call(CategorySeeder::class);
         $this->call(UserSeeder::class);
         $this->call(PostSeeder::class);
         $this->call(CommentSeeder::class);
