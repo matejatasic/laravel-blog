be654fae6ef293c04451df12398adf31c0c290fc
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-10-09 12:40:42 +0200

    Update index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -17,6 +17,17 @@
                     <a href="{{ route('posts.create') }}" class="btn btn-success create mb-3">Create</a>
                 </div>
             @endif
+            <div>
+                <form action="" class="form">
+                    <select name="categories" id="categories">
+                        <option value="-">-</option>
+                        <option value="0">All</option>
+                        @foreach ($categories as $category)
+                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                        @endforeach
+                    </select>
+                </form>
+            </div>
             @foreach ($posts as $post)
                 <div class="blog-post mb-4">
                     <div>
@@ -26,7 +37,7 @@
                         <h2 class="medium">{{ $post->title > 20 ? substr($post->title, 0, 20) . '...' : $post->title }}</h2>
                         <p class="mb-2">By <b>{{ $post->user->name }}</b>, {{ $post->created_at->diffForHumans() }}</p>
                         <p class="lead">
-                            {{ strlen($post->body) > 150 ? substr($post->body, 0, 150) . '...' : $post->body }}
+                            {{ strlen(strip_tags($post->body)) > 150 ? substr(strip_tags($post->body), 0, 150) . '...' : strip_tags($post->body) }}
                         </p>
                         <a href="{{ route('posts.show', $post->id) }}" class="btn btn-primary">Read More</a>
                     </div>
@@ -35,4 +46,32 @@
         </div>
         {{ $posts->links('vendor.custom') }}
     </div>
+@endsection
+
+@section('scripts')
+    @if (!Auth::check())
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    @endif
+    <script>
+        let categories = $('#categories');
+        let id = 0;
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        categories.on('change', (e) => {
+            id = e.target.value;
+            
+            if(id === '0') {
+                window.location.href="/posts";
+            }
+            
+            else {
+                window.location.href=`/posts/category/${id}`;
+            }
+        });
+    </script>
 @endsection
