ace4044818c8803c3b520a64547ac9aec2e671bb
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-10-09 15:38:22 +0200

    Update create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -15,7 +15,7 @@
                 </ul>
             </div>
         @endif
-        <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data" class="form my-3">
+        <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data" id="createForm" class="form my-3">
             @csrf
                 <div class="form-group">
                     <label>Title</label>
@@ -29,8 +29,33 @@
                     <label>Post Image</label>
                     <input type="file" name="img_path">
                 </div>
+                <div class="form-group">
+                    <label>Category</label>
+                    <select name="categories">
+                        @foreach ($categories as $category)
+                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label>Tags</label>
+                    <select class="select-category" name="tags[]" multiple="multiple">
+                        @foreach($tags as $tag)
+                            <option value='{{ $tag->id }}'>{{ $tag->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
                 
-                <input type="submit" class="btn btn-success" value="Create">
+                <input type="submit" id="submit" class="btn btn-success" value="Create">
         </form>
     </div>
+@endsection
+
+@section('scripts')
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+    <script type="text/javascript">
+        $('.select-category').select2();
+    </script>
 @endsection
