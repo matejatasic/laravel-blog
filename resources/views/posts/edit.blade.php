c024136717b1a1dfe9c193f3ffa71c3653cf113c
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-10-11 14:01:20 +0200

    Update edit.blade.php

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.main')
 
+@section('styles')
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+@endsection
+
 @section('content')
     <div class="container">
         <div class="mb-2">
@@ -7,7 +11,7 @@
         </div>
         <hr>
         <div class="my-2">
-            <a href="{{ route('posts.show', $post->id) }}" class="btn btn-warning">Back</a>
+            <a href="{{ route('posts.show', $post->id) }}" class="btn btn-black">Back</a>
         </div>
         @if ($errors->any())
             <div class="alert alert-danger" role="alert">
@@ -18,22 +22,48 @@
                 </ul>
             </div>
         @endif
-        <form action="{{ route('posts.update', $post->id) }}" method="POST" enctype="multipart/form-data" class="form my-3">
+        <form action="{{ route('posts.update', $post->id) }}" method="POST" enctype="multipart/form-data" id="editForm" class="form my-3">
             @csrf
+            @method('PUT')
                 <div class="form-group">
                     <label>Title</label>
-                    <input type="text" name="title" value="{{ $post->title }}">
+                    <input type="text" name="title" id="title" value="{{ $post->title }}">
                 </div>
                 <div class="form-group">
                     <label>Body</label>
-                    <textarea name="body" cols="30" rows="10">{{ $post->body }}</textarea>
+                    <textarea name="body" id="body" cols="30" rows="10">{{ $post->body }}</textarea>
                 </div>
                 <div class="form-group">
                     <label>Post Image</label>
-                    <input type="file" name="img_path">
+                    <input type="file" name="img_path" id="img_path">
+                </div>
+                <div class="form-group">
+                    <label>Category</label>
+                    <select name="category_id">
+                        @foreach ($categories as $category)
+                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label>Tags</label>
+                    <select class="select-category" name="tags[]" multiple="multiple">
+                        @foreach($tags as $tag)
+                            <option value='{{ $tag->id }}'>{{ $tag->name }}</option>
+                        @endforeach
+                    </select>
                 </div>
                 
                 <input type="submit" class="btn btn-success" value="Update">
         </form>
     </div>
+@endsection
+
+@section('scripts')
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(() => {
+            $('.select-category').select2();
+        })
+    </script>
 @endsection
