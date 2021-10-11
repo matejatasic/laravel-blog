b241bed1afcac7c19feef863ceb51ae3aa00d582
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-10-11 14:02:00 +0200

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,6 +13,8 @@
         @if (Auth::check())
             <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
         @endif
+        
+        @yield('styles')
     </head>
     <body>
         <!-- navbar -->
