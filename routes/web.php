11fe508deff21f83329accba7aaefb095f1e370a
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-10-11 15:14:30 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,21 +25,25 @@ Route::get('/about', [PageController::class, 'getAbout'])->name('pages.about');
 Route::get('/contact', [PageController::class, 'getContact'])->name('pages.contact');
 
 // Login & Registration
-Route::get('/login', [AuthController::class, 'getLogin'])->name('getLogin');
-Route::get('/register', [AuthController::class, 'getRegister'])->name('getRegister');
-Route::post('/login', [AuthController::class, 'postLogin'])->name('postLogin');
-Route::post('/register', [AuthController::class, 'postRegister'])->name('postRegister');
-Route::get('/logout', [AuthController::class, 'postLogout'])->name('logout');
+Route::group(['middleware' => 'guest'], function() {
+    Route::get('/login', [AuthController::class, 'getLogin'])->name('getLogin');
+    Route::get('/register', [AuthController::class, 'getRegister'])->name('getRegister');
+    Route::post('/login', [AuthController::class, 'postLogin'])->name('postLogin');
+    Route::post('/register', [AuthController::class, 'postRegister'])->name('postRegister');
+});
+Route::get('/logout', [AuthController::class, 'postLogout'])->middleware('auth')->name('logout');
 
 // Admin
-Route::get('/admin', [AdminController::class, 'dashboard'])->name('admin.dashboard');
-Route::get('/admin/users', [AdminController::class, 'getUsers'])->name('admin.users');
-Route::get('/admin/posts', [AdminController::class, 'getPosts'])->name('admin.posts');
-Route::get('/admin/comments', [AdminController::class, 'getComments'])->name('admin.comments');
-Route::get('/admin/categories', [AdminController::class, 'getCategories'])->name('admin.categories');
-Route::get('/admin/tags', [AdminController::class, 'getTags'])->name('admin.tags');
-Route::get('/admin/likes', [AdminController::class, 'getLikes'])->name('admin.likes');
-Route::get('/admin/posts/{id}', [AdminController::class, 'showPost']);
+Route::group(['middleware' => 'admin'], function() {
+    Route::get('/admin', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::get('/admin/users', [AdminController::class, 'getUsers'])->name('admin.users');
+    Route::get('/admin/posts', [AdminController::class, 'getPosts'])->name('admin.posts');
+    Route::get('/admin/comments', [AdminController::class, 'getComments'])->name('admin.comments');
+    Route::get('/admin/categories', [AdminController::class, 'getCategories'])->name('admin.categories');
+    Route::get('/admin/tags', [AdminController::class, 'getTags'])->name('admin.tags');
+    Route::get('/admin/likes', [AdminController::class, 'getLikes'])->name('admin.likes');
+    Route::get('/admin/posts/{id}', [AdminController::class, 'showPost']);
+});
 
 // Posts
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
@@ -54,10 +58,12 @@ Route::group(['middleware' => 'auth'], function() {
 Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
 
 //Comments
-Route::get('/comments/{id}/create', [CommentController::class, 'create'])->name('comments.create');
-Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
-Route::get('/comments/{id}/edit', [CommentController::class, 'edit'])->name('comments.edit');
-Route::put('/comments/{id}', [CommentController::class, 'update'])->name('comments.update');
-Route::delete('/comments/{id}', [CommentController::class, 'destroy'])->name('comments.destroy');
-Route::post('/comments/{comment}/likes', [CommentLikeController::class, 'store'])->name('comments.likes');
-Route::delete('/comments/{comment}/likes', [CommentLikeController::class, 'destroy'])->name('comments.likes');
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/comments/{id}/create', [CommentController::class, 'create'])->name('comments.create');
+    Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::get('/comments/{id}/edit', [CommentController::class, 'edit'])->name('comments.edit');
+    Route::put('/comments/{id}', [CommentController::class, 'update'])->name('comments.update');
+    Route::delete('/comments/{id}', [CommentController::class, 'destroy'])->name('comments.destroy');
+    Route::post('/comments/{comment}/likes', [CommentLikeController::class, 'store'])->name('comments.likes');
+    Route::delete('/comments/{comment}/likes', [CommentLikeController::class, 'destroy'])->name('comments.likes');
+});
