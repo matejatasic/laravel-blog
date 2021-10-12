39a2ed7cee262227ab6802e40cbee32ca915645f
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-10-12 14:34:39 +0200

    Update posts.blade.php

--- resources/views/admin/posts.blade.php
+++ resources/views/admin/posts.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="mb-2">
-        <h1 class="text-center large">Admin Dashboard</h1>
+        <h1 class="text-center large">Posts</h1>
     </div>
     <hr>
     <div class="my-3">
@@ -18,6 +18,7 @@
                     <th>Image</th>
                     <th>Name</th>
                     <th>Text</th>
+                    <th>Approved</th>
                     <th>Actions</th>
                 </tr>
             </thead>
@@ -28,10 +29,13 @@
                         <td><img src="{{ $post->img_path }}" alt="img_thumbnail"></td>
                         <td>{{ $post->title }}</td>
                         <td>{{ strlen($post->body) > 150 ? substr($post->body, 0, 150) . '...' : $post->body }}</td>
+                        <td>{{ $post->approved === 'approved' ? 'Approved' : 'Unapproved'}}</td>
                         <td>
-                            <button class="btn btn-primary viewBtn" id="{{ $post->id }}">View</button>
-                            <button class="btn btn-success">Approve</button>
-                            <button class="btn btn-danger">Delete</button>
+                            <button class="viewBtn btn btn-primary" id="{{ $post->id }}">View</button>
+                            @if ($post->approved === 'unapproved')
+                                <button class="approveModalBtn  btn btn-success" id="{{ $post->id }}">Approve</button>
+                            @endif
+                            <button class="deleteModalBtn btn btn-danger" id="{{ $post->id }}">Delete</button>
                         </td>
                     </tr>
                 @endforeach
@@ -41,15 +45,13 @@
 
         <!-- modal -->
         <div id="viewModal" class="modal">
-            <div class="modal-content">
-                <div class="modal-header">
+            <div id="modal-content">
+                <div id="modal-header">
                     <span id="close">&times;</span>
-                    <h2 id="post-title"></h2>
+                    <h2 id="modal-title"></h2>
                 </div>
-                <div class="modal-body">
-                    <img src="" alt="" id="post-image">
-                    <p class="post-author"></p>
-                    <p id="post-body"></p>
+                <div id="modal-body">
+                    
                 </div>
             </div>
         </div>
@@ -60,25 +62,67 @@
     <script>
         const modal = $('#viewModal');
         const close = $('#close');
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
 
         $('.viewBtn').click((e) => {
             event.stopPropagation();
             event.stopImmediatePropagation();
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                }
-            });
 
             let id = e.target.id;
             modal.css('display', 'block');
 
             $.get('/admin/posts/' + id, (data) => {
-                $('#post-image').attr('src' , data.data.img_path);
-                $('#post-title').html(data.data.title);
-                $('#post-body').html(data.data.body);
-                console.log(data);
+                let post = data.data[0];
+                let user = data.data[1];
+
+                $('#modal-header').css('background', '#b2a1a1')
+                $('#modal-title').html(post['title']);
+                $('#modal-body').html(`
+                    <img src="${ post['img_path'] }" alt="" id="post-image">
+                    <p id="post-author">Author: <strong>${ user }</strong></p>
+                    <p id="post-body">${ post['body'] }</p>
+                `);
             });
+        });
+        
+        $('.approveModalBtn').click((e) => {
+            event.stopPropagation();
+            event.stopImmediatePropagation();
+
+            let id = e.target.id;
+            modal.css('display', 'block');
+            $('#modal-header').css('background', 'rgb(28, 180, 28)');
+            $('#modal-title').html('Are you sure you want to approve this post?')
+            $('#modal-body').html(`
+                <form action="{{ route('admin.approvePost') }}" method="POST">
+                    @csrf
+                    <input type="hidden" name="id" value="${id}">    
+                    <input type="submit" class="btn btn-success" value="Approve">    
+                </form>
+            `);
+        });
+
+        $('.deleteModalBtn').click((e) => {
+            event.stopPropagation();
+            event.stopImmediatePropagation();
+
+            let id = e.target.id;
+            let route = '{{ route("admin.deletePost", ":id") }}'; 
+            route = route.replace(':id', id);
+            modal.css('display', 'block');
+            $('#modal-header').css('background', 'rgb(204, 55, 55)');
+            $('#modal-title').html('Are you sure you want to delete this post?')
+            $('#modal-body').html(`
+                <form action="${route}" method="POST">
+                    @csrf
+                    @method('DELETE')   
+                    <input type="submit" class="btn btn-danger" value="Delete">    
+                </form>
+            `);
         });
 
         close.click(() => {
