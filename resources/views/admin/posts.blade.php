b840972314e233f16bf612c571bd68d102631eda
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-10-13 11:39:53 +0200

    Update posts.blade.php

--- resources/views/admin/posts.blade.php
+++ resources/views/admin/posts.blade.php
@@ -43,19 +43,7 @@
         </table>
         {{ $posts->links('vendor.custom') }}
 
-        <!-- modal -->
-        <div id="viewModal" class="modal">
-            <div id="modal-content">
-                <div id="modal-header">
-                    <span id="close">&times;</span>
-                    <h2 id="modal-title"></h2>
-                </div>
-                <div id="modal-body">
-                    
-                </div>
-            </div>
-        </div>
-        <!-- !modal -->
+        <x-modal />
 @endsection
 
 @section('scripts')
