bdb158e20de9955d41a654e0cc8ba238136fa4aa
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-10-13 11:39:07 +0200

    Add files via upload

---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,13 @@
+<!-- modal -->
+<div id="viewModal" class="modal">
+    <div id="modal-content">
+        <div id="modal-header">
+            <span id="close">&times;</span>
+            <h2 id="modal-title"></h2>
+        </div>
+        <div id="modal-body">
+            
+        </div>
+    </div>
+</div>
+<!-- !modal -->
