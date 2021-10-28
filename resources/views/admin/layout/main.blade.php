09d3ddd54f1623aa45b5bca6a3a7487f2033b806
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-10-28 17:05:16 +0200

    Update main.blade.php

--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -16,39 +16,41 @@
     </head>
     <body>
         <!-- navbar -->
-        <nav class="navbar bg-dark">
-            <a href="{{ route('pages.home') }}" class="navbar-brand">
-                Laravel Blog
-            </a>
-            <ul class="left-nav">
-                <li>
-                    <a href="{{ route('pages.home') }}">Home</a>
-                </li>
-                <li>
-                    <a href="{{ route('posts.index') }}">Blog</a>
-                </li>
-                <li>
-                    <a href="{{ route('pages.about') }}">About</a>
-                </li>
-                <li>
-                    <a href="{{ route('pages.contact') }}">Contact</a>
-                </li>
-                <li>
-                    <a href="{{ route('admin.dashboard') }}">Dashboard</a>
-                </li>
-            </ul>
+        <nav id="navbar" class="bg-dark">
+            <div id="menu-wrapper">
+                <a href="{{ route('pages.home') }}" class="navbar-brand">
+                    Laravel Blog
+                </a>
+                <ul class="left-nav">
+                    <li>
+                        <a href="{{ route('pages.home') }}">Home</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('posts.index') }}">Blog</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('pages.about') }}">About</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('pages.contact') }}">Contact</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('admin.dashboard') }}">Dashboard</a>
+                    </li>
+                </ul>
 
-            <div class="right-nav">
-                <div class="username-div">
-                    <img src="{{ auth()->user()->img_path }}" alt="avatar" id="avatar">
-                    <a id="username">{{ auth()->user()->name }} <i class="fas fa-caret-down"></i></a>
-                    <div id="dropdown">
-                        <div>
-                            <a href="#">Settings</a>
-                        </div>
-                        <hr>
-                        <div>
-                            <a href="{{ route('logout') }}">Logout</a>
+                <div class="right-nav">
+                    <div class="username-div">
+                        <img src="{{ auth()->user()->img_path }}" alt="avatar" id="avatar">
+                        <a id="username">{{ auth()->user()->name }} <i class="fas fa-caret-down"></i></a>
+                        <div id="dropdown">
+                            <div>
+                                <a href="#">Settings</a>
+                            </div>
+                            <hr>
+                            <div>
+                                <a href="{{ route('logout') }}">Logout</a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -121,5 +123,66 @@
         </script>
 
         @yield('scripts')
+        
+        <!-- footer -->
+        <footer id="footer" class="bg-dark">
+            <div id="footer-links">
+                <div id="pages">
+                    <h3>Pages</h3>
+                    <ul>
+                        <li>
+                            <a href="{{ route('pages.home') }}">Home</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('posts.index') }}">Blog</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('pages.about') }}">About</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('pages.contact') }}">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <div id="find-us">
+                    <h3>Find us</h3>
+                    <ul>
+                        <li>
+                            <a href="#">What we do</a>
+                        </li>
+                        <li>
+                            <a href="#">Address</a>
+                        </li>
+                        <li>
+                            <a href="#">Phone</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <div id="latest-posts">
+                    <h3>Features</h3>
+                    <ul>
+                        <li>
+                            <a href="#">Articles</a>
+                        </li>
+                        <li>
+                            <a href="#">Collections</a>
+                        </li>
+                        <li>
+                            <a href="#">Concepts</a>
+                        </li>
+                        <li>
+                            <a href="#">Tips & Advice</a>
+                        </li>    
+                    </ul>
+                </div>
+            </div>
+            <p>
+                Copyright 2021 Mateja Tasic. All Rights Reserved
+            </p>
+        </footer>
+        <!-- !footer -->
     </body>
 </html>
