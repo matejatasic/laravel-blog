56a5b44aff4d52846c6436d4cd650bd8535ee504
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-10-29 17:14:48 +0200

    Update home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -32,8 +32,8 @@
             </div>
             <div class="px-3">
                 <h2>{{ $latest_post->title > 20 ? substr($latest_post->title, 0, 20) . '...' : $latest_post->title }}</h2>
-                <p class="mb-2">By <b>{{ $latest_post->user->name }}</b>, {{ $latest_post->created_at->diffForHumans() }}</p>
-                <p class="lead">
+                <span class="mb-2">By <b>{{ $latest_post->user->name }}</b>, {{ $latest_post->created_at->diffForHumans() }}</span>
+                <p>
                     {{ strlen(strip_tags($latest_post->body)) > 150 ? substr(strip_tags($latest_post->body), 0, 150) . '...' : strip_tags($latest_post->body) }}
                 </p>
                 <a href="{{ route('posts.show', $latest_post->id) }}" class="btn btn-primary mb-2">Read More</a>
