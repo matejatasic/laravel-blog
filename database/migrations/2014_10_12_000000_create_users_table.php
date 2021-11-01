d3aa38e1b7d061e62f82748d94a9cd4fce5412b5
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-11-01 16:15:07 +0100

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('img_path');
+            $table->string('token')->nullable();
             $table->enum('role', ['user', 'admin'])->default('user');
             $table->enum('status', ['active', 'banned'])->default('active');
             $table->rememberToken();
